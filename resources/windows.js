6f9f8a2c5b87f3067b7717864d71839c373095ae
vistyy <sajmonpele@gmail.com>
2020-03-21 20:21:59 +0100

    changed login window

--- resources/windows.js
+++ resources/windows.js
@@ -1,12 +1,17 @@
-function test(){
-    el = document.getElementById('login-window');
-    if (el.className == "collapsed") {
-        el.className = "loginwindow";
-    }else{
-        el.className = "collapsed";
-    }
+function test() {
+  el = document.getElementById("login-window");
+  mainContainer = document.getElementById("main-container");
+  if (el.classList.contains("collapsed")) {
+    el.classList.toggle("loginwindow");
+    el.classList.toggle("collapsed");
+    mainContainer.style.opacity = "0.7";
+  } else {
+    el.classList.toggle("loginwindow");
+    el.classList.toggle("collapsed");
+    mainContainer.style.opacity = "1";
+  }
 }
 
-function submit(){
-    document.getElementById("loginform").submit();
+function submit() {
+  document.getElementById("loginform").submit();
 }
