5ceff001cc0cf0a07e962f25a015d079e5e3458c
Sebastian <sebastian@localhost.localdomain>
2020-04-11 17:05:19 +0200

    added logging

--- resources/sign-in.php
+++ resources/sign-in.php
@@ -1,3 +1,36 @@
 <?php
 
-echo "reee";
+$username=$_POST["username"];
+$pass = $_POST["pass"];
+$data = get_data($username);
+$hash = hash_pass($pass, $data["salt"]);
+if ($hash == $data["passhash"]){
+    echo "reee";
+}
+
+
+function hash_pass($password, $salt){
+    $hash = crypt($password,$salt);
+    return $hash;
+}
+
+
+function get_data($user){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    $conn = new mysqli($servername, $username, $password, "css");
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $query = "SELECT * FROM users WHERE username='$user'";
+    $result=mysqli_query($conn, $query);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $data=array("username"=>$row["username"],"salt"=> $row["salt"],"passhash"=> $row["passhash"]);
+        }
+    }
+    return $data;
+}
--- resources/sign-up.php
+++ resources/sign-up.php
@@ -1,4 +1,47 @@
 <?php
 
+$username=$_POST["username"];
+$pass = $_POST["pass"];
+$pass2 = $_POST["pass2"];
+echo ("$username, $pass, $pass2 <br>");
 
-echo ("reeee");
+if($pass != $pass2){
+    header ('Location:../index.html');
+};
+
+$salt = gen_salt(15);
+$hash = gen_hash($salt,$pass);
+upload_to_db($hash, $salt,$username);
+
+
+function gen_salt($n){
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+    $randomString = ''; 
+    for ($i = 0; $i < $n; $i++){ 
+        $index = rand(0, strlen($characters) - 1); 
+        $randomString .= $characters[$index]; 
+    } 
+    return $randomString; 
+};
+
+
+function gen_hash($salt, $password){
+    $hash = crypt($password,$salt);
+    return $hash;
+}
+
+function upload_to_db($hash,$salt, $user){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    $conn = new mysqli($servername, $username, $password, "css");
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $query = "INSERT INTO users (username,salt, passhash) VALUES ('$user', '$salt', '$hash')";
+    mysqli_query($conn, $query);
+}
+
+
