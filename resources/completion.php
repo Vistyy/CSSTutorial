6d1c01873e2bf93c23d98f0e457e1a8ab1fdd6c9
Sebastian <sebastian@localhost.localdomain>
2020-04-12 23:33:21 +0200

    reee

--- resources/completion.php
+++ resources/completion.php
@@ -1,25 +1,4 @@
 <?php
-<<<<<<< HEAD
-session_start();
-$servername = "172.17.0.3";
-$usernamedb = "root";
-$passworddb = "dupa12";
-$username = $_SESSION['username'];
-
-$conn = new mysqli($servername, $usernamedb, $passworddb, "csstutorial");
-
-var_dump($_POST);
-
-
-if(isset($_POST['completion-flexbox'])){
-    $query = "UPDATE progress set flexbox=true where username= '$username' ";
-    mysqli_query($conn, $query);
-}
-
-// przy logowaniu ustawic sesje na te checkmarki, ustawiac w index php jezeli istnieje to je zaznaczyc
-// przy rejestracji stworzyc tez tabele progress
-?>
-=======
 
 session_start();
 $username=$_SESSION['username'];
@@ -53,4 +32,3 @@ if(isset($_POST['completion-media'])){
  header('Location:../index.php');
 
 ?>
->>>>>>> 181842228a6ffe8c94b69421913f644d43cfa7ed
