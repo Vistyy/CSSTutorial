b158c17cc586b1d9ecc0b53d4821a4da3a46192c
Sebastian <sebastian@localhost.localdomain>
2020-04-12 23:15:04 +0200

    completion

--- resources/completion.php
+++ resources/completion.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-$username=$_SESSION["username"];
+$username=$_SESSION['username'];
 $servername = "172.17.0.3";
 $usernamedb = "root";
 $passworddb = "dupa12";
@@ -28,6 +28,6 @@ if(isset($_POST['completion-media'])){
     $query = "UPDATE progress set media=1 WHERE username='$username'";
     mysqli_query($conn, $query);
 }
-header('Location:../index.php');
+// header('Location:../index.php');
 
 ?>
