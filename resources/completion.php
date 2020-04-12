de6ce14cf35bfb7f76f21ab4047c5eaf835f8019
Sebastian <sebastian@localhost.localdomain>
2020-04-12 23:19:34 +0200

    completion

--- resources/completion.php
+++ resources/completion.php
@@ -2,6 +2,7 @@
 
 session_start();
 $username=$_SESSION['username'];
+echo $username;
 $servername = "172.17.0.3";
 $usernamedb = "root";
 $passworddb = "dupa12";
@@ -10,17 +11,17 @@ $conn = new mysqli($servername, $usernamedb, $passworddb, "csstutorial");
 
 
 if(isset($_POST['completion-flexbox'])){
-   $query = "UPDATE set progress flexbox=1 WHERE username='$username'";
+   $query = "UPDATE  progress set flexbox=1 WHERE username='$username'";
    mysqli_query($conn, $query);
 
 }
 if(isset($_POST['completion-effects'])){
-    $query = "UPDATE set progress text=1 WHERE username='$username'";
+    $query = "UPDATE  progress  set text=1 WHERE username='$username'";
     mysqli_query($conn, $query);
 }
 
 if(isset($_POST['completion-anim'])){
-    $query = "UPDATE set progress animations=1 WHERE username='$username'";
+    $query = "UPDATE  progress set animations=1 WHERE username='$username'";
     mysqli_query($conn, $query);
 }
 
