1c91ce1f77df6def6caaffe87f1f3cd96b0792e2
Sebastian <sebastian@localhost.localdomain>
2020-04-12 23:52:25 +0200

    profile

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
                <!--  -->
                <form action="resources/completion.php" method="post">
               <label class="completion-container">
-                <input type="checkbox" onclick="this.form.submit()" name="completion-anim" <?php if(isset($_SESSION['completion-anim"'])) echo "checked"; ?>   />
+                <input type="checkbox" onclick="this.form.submit()" name="completion-anim" <?php if(isset($_SESSION['completion-anim'])) echo "checked"; ?>   />
                 <span class="checkmark"></span>
               </label>r
               </form>
@@ -116,7 +116,7 @@
                <!--  -->
                <form action="resources/completion.php" method="post">
               <label class="completion-container">
-                <input type="checkbox" onclick="this.form.submit()"  name="completion-flexbox" <?php if(isset($_SESSION['completion-flexbox"'])) echo "checked"; ?>/>
+                <input type="checkbox" onclick="this.form.submit()"  name="completion-flexbox" <?php if(isset($_SESSION['completion-flexbox'])) echo "checked"; ?>/>
                 <span class="checkmark"></span>
               </label>
               </form>
@@ -140,7 +140,7 @@
               <!--  -->
               <form action="resources/completion.php" method="post">
               <label class="completion-container">
-                <input type="checkbox" onclick="this.form.submit()" name="completion-media" <?php if(isset($_SESSION['completion-media"'])) echo "checked"; ?> checked/>
+                <input type="checkbox" onclick="this.form.submit()" name="completion-media" <?php if(isset($_SESSION['completion-media'])) echo "checked"; ?> checked/>
                 <span class="checkmark"></span>
               </label>
               </form>
@@ -163,7 +163,7 @@
                <!--  -->
                <form action="resources/completion.php" method="post">
               <label class="completion-container">
-                <input type="checkbox" onclick="this.form.submit()" name="completion-effects" <?php if(isset($_SESSION['completion-effects"'])) echo "checked"; ?> />
+                <input type="checkbox" onclick="this.form.submit()" name="completion-effects" <?php if(isset($_SESSION['completion-effects'])) echo "checked"; ?> />
                 <span class="checkmark"></span>
               </label>
               </form>
